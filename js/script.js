39943371f043e973c88955461c8dbce7f08ab946
Alberto Zordan <zorberto@gmail.com>
2020-07-21 14:43:49 +0200

    Some variants and tweaks...

--- js/script.js
+++ js/script.js
@@ -13,8 +13,10 @@
 
 
 // *** VARIANTE BONUS ***//
-// Inizializzo una variabile in cui salvare il livello di difficoltà
-var difficulty = parseInt(prompt('Inserisci il livello di difficoltà: 0 - Facile, 1 - Medio, 2 - Difficile','0 - 1 - 2'));
+// Inizializzo una variabile in cui salvare il livello di difficoltà, su cui effettuo un controllo dell'input dell'utente con un ciclo do-while
+do {
+    var difficulty = parseInt(prompt('Inserisci il livello di difficoltà: 0 - Facile, 1 - Medio, 2 - Difficile','0 - 1 - 2'));
+} while (difficulty > 2 || difficulty < 0);
 
 // Creo una variabile che memorizza l'estremo superiore dell'intervallo del gioco, variabile a seconda dell'input dell'utente
 var upperBound;
@@ -30,15 +32,20 @@ switch (difficulty) {
     case 2:
         upperBound = 50;
         break;
-    default: alert('Scegli uno dei seguenti numeri: 0, 1, 2');
+    // default: alert('Scegli uno dei seguenti numeri: 0, 1, 2');
+    // se faccio il controllo dell'input di 'difficulty' non serve default
 }
+
+// Creo la variabile 'tentativi' per l'utente
+var tentativi = upperBound - 16;
 console.log('Difficoltà:',difficulty);
 
-// 1. Genero 16 numeri casuali nell'intervallo [1,upperBound] e li salvo in un array, usando la funzione che controlla eventuali ripetizioni. In questo caso utilizzo il ciclo while perché, pur conoscendo la lunghezza finale dell'array, non so quante volte verrà effettuato il ciclo, per via del controllo sulle ripetizioni che blocca il push del numero doppione
+// 1. Genero 16 numeri casuali nell'intervallo [1,upperBound] e li salvo in un array, usando la funzione che controlla eventuali ripetizioni. In questo caso utilizzo il ciclo while perché, pur conoscendo la lunghezza finale dell'array, non so quante volte verrà effettuato il ciclo, per via del controllo sulle ripetizioni che blocca il push del numero già presente
 
 var arrPc = [];
+var numPc;
 while (arrPc.length < 16) {
-    var numPc = numRandom(1,upperBound);
+    numPc = numRandom(1,upperBound);
     if (!checkNum(arrPc,numPc)) {
         arrPc.push(numPc);
     }
@@ -50,24 +57,51 @@ console.log('Array PC:',arrPc);
 
 // Inizializzo l'array dell'utente e la variabile che terrà conto del punteggio ottenuto (numero di volte di inserimenti consentiti)
 
+// var arrUser = [];
+// var numUser;
+// var punteggio = 0;
+// do {
+//     // Chiedo all'utente tanti numeri quanti stabiliti dalla var 'tentativi' nell'intervallo [1,upperBound]
+//     numUser = parseInt(prompt('Inserisci un numero da 1 a ' + upperBound,'1-' + upperBound));
+//     // Effettuo il controllo che siano all'interno dell'intervallo stabilito e che non siano stati precedentemente inseriti. A esito positivo inserisco il numero nell'array dell'utente e aggiorno la variabile del punteggio
+//     if (!checkNum(arrUser,numUser) && numUser >= 1 && numUser <= upperBound) {
+//         arrUser.push(numUser);
+//         punteggio++;
+//     }   else {
+//         alert('Sono ammessi solo numeri nell\'intervallo [1,' + upperBound + '] che non hai già inserito');
+//     }
+// // fino a quando non raggiungo la lunghezza max dell'array dell'utente o il numero inserito corrisponde a un elemento dell'array del pc (bomba!)
+// } while (arrUser.length < tentativi && !checkNum(arrPc,numUser));
+
+// *** Con WHILE e l'ausilio della variabile sentinella 'trovato' posso evitare di pushare l'elemento che fa scattare la bomba *** //
 var arrUser = [];
-var punteggio = 0;
-do {
-    // Chiedo all'utente (upperBound-16) numeri nell'intervallo [1,upperBound]
-    var numUser = parseInt(prompt('Inserisci un numero da 1 a ' + upperBound,'1-' + upperBound));
-    // Effettuo il controllo che siano all'interno dell'intervallo stabilito e che non siano stati precedentemente inseriti. A esito positivo inserisco il numero nell'array dell'utente e aggiorno la variabile del punteggio
-    if (!checkNum(arrUser,numUser) && numUser >= 1 && numUser <= upperBound) {
-        arrUser.push(numUser);
-        punteggio++;
-    }   else {
-        alert('Sono ammessi solo numeri nell\'intervallo [1,' + upperBound + '] che non hai già inserito');
+var trovato = false;
+
+while (arrUser.length < tentativi && trovato == false) {
+    numUser = parseInt(prompt('Inserisci un numero da 1 a ' + upperBound,'1-' + upperBound));
+    while (numUser <= 0 || numUser > upperBound) { //oppure (!(numUser > 0 && numUser <= upperBound))
+        numUser = parseInt(prompt('Sono ammessi solo numeri nell\'intervallo [1,' + upperBound + ']'));
     }
-// fino a quando non raggiungo la lunghezza max dell'array dell'utente o il numero inserito corrisponde a un elemento dell'array del pc (bomba!)
-} while (arrUser.length < (upperBound - 16) && !checkNum(arrPc,numUser));
+    if (checkNum(arrPc,numUser)) {
+        trovato = true;
+    }   else if (!checkNum(arrUser,numUser)) {
+        arrUser.push(numUser);
+    }   else {
+        alert('Attenzione! Il numero è già presente, inseriscine un altro');
+    }
+}
+
+if (trovato) {
+    alert('Boom! Hai beccato una bomba, mi spiace. Hai totalizzato ' + arrUser.length + ' punti');
+}   else {
+    alert('Hai vinto!');
+}
 
 console.log('Array utente:',arrUser);
 
-console.log('Boom! Hai beccato una bomba, mi spiace. Hai totalizzato',--punteggio,'punti');
+// *** Alert per il ciclo do-while con la variabile 'punteggio' *** //
+// alert('Boom! Hai beccato una bomba, mi spiace. Hai totalizzato',--punteggio,'punti');
+// alert('Boom! Hai beccato una bomba, mi spiace. Hai totalizzato',arrUser.length - 1,'punti');
 
 
 // *** FUNZIONI *** //
